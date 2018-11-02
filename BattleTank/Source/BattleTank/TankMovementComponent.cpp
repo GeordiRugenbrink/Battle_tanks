bf2e4981c2c4e8920cdf8722c2e36c0c81b47799
GAME-PC\Geordi <G.Rugenbrink@kpnmail.nl>
2018-11-02 13:26:18 +0100

    Added documentation to the movementcomponent class

--- BattleTank/Source/BattleTank/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/TankMovementComponent.cpp
@@ -3,22 +3,35 @@
 #include "TankMovementComponent.h"
 #include "TankTrack.h"
 
+
+/*
+Initializes the Tracks of the tank
+*/
 void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet) {
 	if (!LeftTrackToSet || !RightTrackToSet) { return; }
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
 
+/*
+Makes tank move forward by setting the same force on both the tracks
+*/
 void UTankMovementComponent::IntendMoveForward(float Throw) {
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
 }
 
+/*
+Makes the tank turn by setting a negative force on one of the tracks and a positive in the other one
+*/
 void UTankMovementComponent::IntendTurnRight(float Throw) {
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(-Throw);
 }
 
+/*
+Calculates what way the AI tanks need to move to get to the player and let's them move towards the player
+*/
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed) {
 	auto TankForwardDirection = GetOwner()->GetActorForwardVector().GetSafeNormal();
 	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
