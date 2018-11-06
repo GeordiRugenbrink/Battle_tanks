d83702fae5febeeaa45444c351b38df893c5a372
GAME-PC\Geordi <G.Rugenbrink@kpnmail.nl>
2018-11-06 14:39:54 +0100

    Created a way to find the aimingcomponent using Blueprint

--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -12,6 +12,15 @@
 ATank::ATank() {
 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+
+	auto TankName = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s YEET: Tank C++ Contruct"), *TankName);
+}
+
+void ATank::BeginPlay() {
+	Super::BeginPlay(); //needed for BP to run
+	auto TankName = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s YEET: Tank C++ BeginPlay"), *TankName);
 }
 
 void ATank::AimAt(FVector HitLocation) {
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -8,6 +8,7 @@
 
 
 class ATank;
+class UTankAimingComponent;
 /**
  *
  */
@@ -18,6 +19,9 @@ class BATTLETANK_API ATankPlayerController : public APlayerController {
 protected:
 	UFUNCTION(BlueprintCallable, Category = "Setup")
 		ATank* GetControlledTank() const;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Setup")
+		void FoundAimingComponent(UTankAimingComponent* AimCompRef);
 
 public:
 	virtual void BeginPlay() override;
