2482859d7c0db5df173da5b96fe82182c38719ac
GAME-PC\Geordi <G.Rugenbrink@kpnmail.nl>
2018-11-16 13:48:28 +0100

    Added an impact particle for the projectile

--- BattleTank/Source/BattleTank/Projectile.h
+++ BattleTank/Source/BattleTank/Projectile.h
@@ -20,19 +20,22 @@ public:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 	void LaunchProjectile(float Speed);
 
 protected:
 	UProjectileMovementComponent* ProjectileMovement = nullptr;
 
 private:
+	UFUNCTION()
+		void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+			UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
+
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 		UStaticMeshComponent* CollisionMesh = nullptr;
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 		UParticleSystemComponent* LaunchBlast = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+		UParticleSystemComponent* ImpactBlast = nullptr;
 };
