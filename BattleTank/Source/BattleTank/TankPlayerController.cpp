6b45cbeb56e22c6045b57da4f803eaddb3d3a3d6
GAME-PC\Geordi <G.Rugenbrink@kpnmail.nl>
2018-11-22 20:54:39 +0100

    The player can now change from 3rd person mode to 1st person mode

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -57,7 +57,7 @@ bool ATankPlayerController::GetLookVectorHitLocation(FVector& HitLocation, FVect
 	FHitResult HitResult;
 	auto StartLocation = PlayerCameraManager->GetCameraLocation();
 	auto EndLocation = StartLocation + (LookDirection * LineTraceRange);
-	if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_Visibility)) {
+	if (GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_Camera)) {
 		HitLocation = HitResult.Location;
 		return true;
 	}
